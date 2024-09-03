62e7e236ce133dbadcec1170a9f127986f7235d6
CSPSY <1554830585@qq.com>
2024-09-03 10:56:56 +0800

    feat: alias配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [
@@ -12,11 +13,11 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    })
+    }) 
   ],
   resolve: {
     alias: {
-      
+      '@': fileURLToPath(new URL('src', import.meta.url))
     }
   },
   server: {
