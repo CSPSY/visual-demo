2e408e8fefb906bf04cfcf768248199dbbe5c241
CSPSY <1554830585@qq.com>
2024-09-03 14:37:44 +0800

    feat: webgl 基础入门---三角形

--- src/views/webgl/webgl-basis/polygon.vue
+++ src/views/webgl/webgl-basis/polygon.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+// 创建一个 ref 用于 canvas 元素
+const canvasRef = ref<HTMLCanvasElement | null>(null);
 </script>
 
 <template>
+  <canvas ref="canvasRef" width="512" height="512"></canvas>
 </template>
 
 <style scoped>
